777dfff895ce52a789a9fbb8cf1f3b6e730d2415
luoluodaduan <63093569+luoluodaduan@users.noreply.github.com>
2026-09-09 06:03:48 +0800

    fix

--- infra/conf/serial/loader.go
+++ infra/conf/serial/loader.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 	"io"
 
-	"github.com/ghodss/yaml"
-	"github.com/pelletier/go-toml"
+	"go.yaml.in/yaml/v3"
+	"github.com/pelletier/go-toml/v2"
 	"github.com/xtls/xray-core/common/errors"
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
@@ -139,16 +139,21 @@ func LoadTOMLConfig(reader io.Reader) (*core.Config, error) {
 }
 
 // DecodeYAMLConfig reads from reader and decode the config into *conf.Config
-// using github.com/ghodss/yaml to convert yaml to json.
+// using go.yaml.in/yaml and map to convert yaml to json.
 func DecodeYAMLConfig(reader io.Reader) (*conf.Config, error) {
 	yamlFile, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, errors.New("failed to read config file").Base(err)
 	}
 
-	jsonFile, err := yaml.YAMLToJSON(yamlFile)
+	configMap := make(map[string]interface{})
+	if err := yaml.Unmarshal(yamlFile, &configMap); err != nil {
+		return nil, errors.New("failed to convert yaml to map").Base(err)
+	}
+
+	jsonFile, err := json.Marshal(&configMap)
 	if err != nil {
-		return nil, errors.New("failed to convert yaml to json").Base(err)
+		return nil, errors.New("failed to convert map to json").Base(err)
 	}
 
 	return DecodeJSONConfig(bytes.NewReader(jsonFile))
